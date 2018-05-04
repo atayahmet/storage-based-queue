b1817fcac634b10683ad09619fc9a2e5f5800d89
Ahmet ATAY <ahmet@efabrika.com>
2018-05-04 16:37:22 +0300

    Emojis replaced with ascii characters in console. Console outputs improved

--- src/console.js
+++ src/console.js
@@ -10,31 +10,37 @@ export function log(...args: any) {
 
 export function taskAddedLog([task]: any[]) {
   log(
-    `%c(${task.handler}) -> ${obj.get(logEvents, 'queue.created')}`,
+    `%c${String.fromCharCode(43)} (${task.handler}) -> ${obj.get(logEvents, 'queue.created')}`,
     'color: green;font-weight: bold;',
   );
 }
 
 export function queueStartLog([type]: any[]) {
   log(
-    `%c(${type}) -> ${obj.get(logEvents, 'queue.starting')}`,
+    `%c${String.fromCharCode(8211)} (${type}) -> ${obj.get(logEvents, 'queue.starting')}`,
     'color: #3fa5f3;font-weight: bold;',
   );
 }
 
 export function nextTaskLog([type]: any[]) {
-  log(`%c(${type}) -> ${obj.get(logEvents, 'queue.next')}`, 'color: #3fa5f3;font-weight: bold;');
+  log(
+    `%c${String.fromCharCode(187)} (${type}) -> ${obj.get(logEvents, 'queue.next')}`,
+    'color: #3fa5f3;font-weight: bold;',
+  );
 }
 
 export function queueStoppingLog([type]: any[]) {
   log(
-    `%c(${type}) -> ${obj.get(logEvents, 'queue.stopping')}`,
+    `%c${String.fromCharCode(8226)} (${type}) -> ${obj.get(logEvents, 'queue.stopping')}`,
     'color: #ff7f94;font-weight: bold;',
   );
 }
 
 export function queueStoppedLog([type]: any[]) {
-  log(`%c(${type}) -> ${obj.get(logEvents, 'queue.stopped')}`, 'color: #ff7f94;font-weight: bold;');
+  log(
+    `%c${String.fromCharCode(8226)} (${type}) -> ${obj.get(logEvents, 'queue.stopped')}`,
+    'color: #ff7f94;font-weight: bold;',
+  );
 }
 
 export function queueEmptyLog([type]: any[]) {
@@ -51,13 +57,20 @@ export function eventFiredLog([key, name]: any[]) {
 
 export function notFoundLog([name]: any[]) {
   log(
-    `%c(${name}) -> ${obj.get(logEvents, 'queue.not-found')}`,
+    `%c${String.fromCharCode(215)} (${name}) -> ${obj.get(logEvents, 'queue.not-found')}`,
     'color: #a0dc3c;font-weight: bold;',
   );
 }
 
-export function workerRunninLog([worker, workerInstance, task, deps]: any[]) {
+export function workerRunninLog([
+  worker: Function,
+  workerInstance,
+  task,
+  channel: string,
+  deps: { [string]: any[] },
+]: any[]) {
   console.groupCollapsed(`${worker.name} - ${task.label}`);
+  log(`%cchannel: ${channel}`, 'color: blue;');
   log(`%clabel: ${task.label || ''}`, 'color: blue;');
   log(`%chandler: ${task.handler}`, 'color: blue;');
   log(`%cpriority: ${task.priority}`, 'color: blue;');
@@ -74,16 +87,16 @@ export function workerRunninLog([worker, workerInstance, task, deps]: any[]) {
 
 export function workerDoneLog([result, task, workerInstance]: any[]) {
   if (result === true) {
-    log('%cTask completed!', 'color: green;');
+    log(`%c${String.fromCharCode(10003)} Task completed!`, 'color: green;');
   } else if (!result && task.tried < workerInstance.retry) {
     log('%cTask will be retried!', 'color: #d8410c;');
   } else {
-    log('%cTask failed and freezed!', 'color: #ef6363;');
+    log(`%c${String.fromCharCode(10005)} Task failed and freezed!`, 'color: #ef6363;');
   }
   console.groupEnd();
 }
 
 export function workerFailedLog() {
-  log('%cTask failed!', 'color: red;');
+  log(`%c${String.fromCharCode(10005)} Task failed!`, 'color: red;');
   console.groupEnd();
 }
